9fe40c1a5b0c839f1102fa280526cf7bd7897752
minnehkamau <minnieches08@gmail.com>
2023-01-16 16:19:44 +0300

    holding new data

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -8,13 +8,25 @@ const [formInput, setFormInput] = useState({
   amount:"",
 });
 
+function handleSubmit(e) {
+  e.preventDefault();
 
+  fetch("http://localhost:8001/transactions",{
 
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(formInput),
+})
+  .then(response => response.json())
+  .then(data => addedData(data))
+  }
 
 
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form onSubmit = {handleSubmit}className="ui form">
         <div className="inline fields">
           <input type="date" name="date" />
           <input type="text" name="description" placeholder="Description" />
