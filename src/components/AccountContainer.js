38ea994b185ba5a1faaab4103963080f0d3e6c9b
minnehkamau <minnieches08@gmail.com>
2023-01-16 10:16:29 +0300

    dbjson data fetching

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+
+const [transaction, setTransaction] = useState([]);
+useEffect(() => { 
+  fetch("http://localhost:8001/transactions")
+  .then(response => response.json())
+  .then((data) => console.log(data));
+}), []
+
+
   return (
     <div>
       <Search />
